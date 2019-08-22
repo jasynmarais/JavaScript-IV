d048d0832b3b26e09fdc1e4718cd88711d52282c
Jasyn Marais <jasynmarais@gmail.com>
2019-08-22 14:14:13 +0200

    refactor task 2

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -22,9 +22,39 @@ class Person3 {
 		return `${this.name} just ate ${edible}.`;
 	}
 	poop() {
-		this.stomach = []; // this.stomach.length = 0;
+		this.stomach = [];
 		return `Stomach is empty now.`;
 	}
 }
 var me = new Person3('Jasyn', 38);
-console.log(me.greet(), me.eat('Rice'), me.poop());
+console.log(me.greet(), me.eat('Biltong'), me.poop());
+
+
+/* TASK 2 */
+class Car {
+	constructor(model, make) {
+		this.model = model;
+		this.make = make;
+		this.odometer = 0;
+		this.canDrive = true;
+	}
+	drive(distance) {
+		if (this.canDrive) {
+			this.odometer += Number(distance);
+			return `Drove ${distance} miles. Odometer: ${this.odometer}.`;
+		}
+		return `I crashed at ${this.odometer} miles!`;
+	}
+	crash() {
+		this.canDrive = false;
+		return `I just crashed.`;
+	}
+	repair() {
+		this.canDrive = true;
+		return `I've been repaired.`;
+	}
+}
+var car = new Car('2006', 'Opel');
+console.log(car.drive(10), car.crash(), car.repair());
+
+
