ff4b48aaa882a6535a5d47cdc2b85ca7b91c795d
Jasyn Marais <jasynmarais@gmail.com>
2019-10-17 16:04:48 +0200

    Add Person to lambda-classes.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+/*
+#### Person
+* First we need a Person class. This will be our `base-class`
+* Person receives `name` `age` `location` all as props
+* Person receives `speak` as a method.
+* This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+*/
+class Person {
+	constructor(data) {
+		this.name = data.name;
+		this.age = data.age;
+		this.location = data.location;
+	}
+	speak() {
+		console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+		return `Hello my name is ${this.name}, I am from ${this.location}`;
+	}
+}
